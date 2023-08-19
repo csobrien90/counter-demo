d655549b4e8c21d8be2c50d4423fdf72034ef2f3
Chad OBrien <obrien.music@gmail.com>
2023-08-18 23:57:35 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# Counter Demo
+
+*A demo project for the Code Louisville Web Development pathway*
+
+This project is a series of three simple counters using three different implementations: functional, OOP, and minimalist (still functional). Each allows you to increment and decrement a number. 
+
+## Questions
+
+### Functional
+
+1. Does it work?
+2. Is it accessible?
+3. Is it safe? (secure and no undefined behavior)
+4. How can we make this shorter?
+
+### Object-Oriented
+
+1. Is it legible?
+2. Is it maintainable?
+3. Is it reusable and extensible?
+4. Is it testable?
+
+### Minimalist
+
+1. Explain this implementation in plain English.
+2. What are the pros and cons of this implementation?
+3. How short is too short?
+4. Is it possible to make this even shorter?
---
+++ scripts.js
@@ -0,0 +1,74 @@
+// ---------------------
+//  Functional Counter
+// ---------------------
+
+// Get DOM Elements
+const functionalCounter = document.getElementById('functional-counter')
+const decrementBtn = functionalCounter.querySelector('.decrement')
+const incrementBtn = functionalCounter.querySelector('.increment')
+const countSpan = functionalCounter.querySelector('.count')
+
+// Set initial count
+let count = 0
+
+// Add event listeners
+decrementBtn.addEventListener('click', (e) => {
+	e.preventDefault()
+	count--
+	countSpan.textContent = count
+})
+
+incrementBtn.addEventListener('click', (e) => {
+	e.preventDefault()
+	count++
+	countSpan.textContent = count
+})
+
+
+// ---------------------
+//      OOP Counter
+// ---------------------
+
+class Counter {
+	constructor(element) {
+		this.element = element
+		this.count = 0
+		this.decrementBtn = element.querySelector('.decrement')
+		this.incrementBtn = element.querySelector('.increment')
+		this.countSpan = element.querySelector('.count')
+		this.addEventListeners()
+	}
+
+	addEventListeners() {
+		this.decrementBtn.addEventListener('click', (e) => {
+			e.preventDefault()
+			this.decrement()
+		})
+
+		this.incrementBtn.addEventListener('click', (e) => {
+			e.preventDefault()
+			this.increment()
+		})
+	}
+
+	increment() {
+		this.count++
+		this.countSpan.textContent = this.count
+	}
+
+	decrement() {
+		this.count--
+		this.countSpan.textContent = this.count
+	}
+}
+
+const classCounter = new Counter(document.getElementById('class-counter'))
+
+
+// ---------------------
+//  Minimalist Counter
+// ---------------------
+
+const [d, p, i] = document.getElementById('minimalist-counter').children
+const op = (e) => {p.firstElementChild.textContent = +p.firstElementChild.textContent + (e.target === d ? -1 : 1)}
+[d, i].forEach(el => el.addEventListener('click', op))
